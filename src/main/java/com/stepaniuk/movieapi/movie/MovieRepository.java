db62412d4d71390842fb39a3a05c5bff2230c6dc
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 13:36:54 +0200

    feat(movie): Added JpaSpecificationExecutor to MovieRepository and deleted unnecessary methods

--- src/main/java/com/stepaniuk/movieapi/movie/MovieRepository.java
+++ src/main/java/com/stepaniuk/movieapi/movie/MovieRepository.java
@@ -1,15 +1,11 @@
 package com.stepaniuk.movieapi.movie;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface MovieRepository extends JpaRepository<Movie,Long>,
+    JpaSpecificationExecutor<Movie> {
 
-public interface MovieRepository extends JpaRepository<Movie,Long> {
-
-    Page<Movie> findByTitleContaining(String title, Pageable pageable);
-    @Query("SELECT m FROM movies m WHERE :genre MEMBER OF m.genres")
-    Page<Movie> findByGenre(@Param("genre") Genre genre, Pageable pageable);
 }
