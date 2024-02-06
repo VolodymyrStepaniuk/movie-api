830ae65eabae22904b1715596270755cf87f38fc
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 13:41:16 +0200

    feat(movie): Rewritten MovieController

--- src/main/java/com/stepaniuk/movieapi/movie/MovieController.java
+++ src/main/java/com/stepaniuk/movieapi/movie/MovieController.java
@@ -1,69 +1,44 @@
 package com.stepaniuk.movieapi.movie;
 
 import com.stepaniuk.movieapi.movie.payload.request.CreateMovieRequest;
-import com.stepaniuk.movieapi.movie.payload.response.MovieListResponse;
 import com.stepaniuk.movieapi.movie.payload.response.MovieResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/movie")
+@RequestMapping(path = "/movie", produces = "application/json")
 @RequiredArgsConstructor
-public class MovieController{
-    private final MovieService service;
-    @GetMapping("/all")
-    public ResponseEntity<MovieListResponse> getAll() {
-        return ResponseEntity.ok(service.getAll());
-    }
-    @GetMapping("/{id}")
-    public ResponseEntity<MovieResponse> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(service.getByIdToResponse(id));
-    }
-    @PostMapping
-    public ResponseEntity<MovieResponse> create(@RequestBody CreateMovieRequest request) {
-        return new ResponseEntity<>(service.create(request), HttpStatus.CREATED);
-    }
-//    @PutMapping("/{id}")
-//    public ResponseEntity.BodyBuilder changeById(@PathVariable Long id){
-//        service.update(service.getById(id));
-//        return ResponseEntity.ok();
-//    }
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity.HeadersBuilder<?> deleteById(@PathVariable Long id){
-//        service.delete(service.getById(id));
-//        return ResponseEntity.ok();
-//    }
+public class MovieController {
 
-    @SneakyThrows
-    @PostMapping("/{movieId}/upload-image")
-    public ResponseEntity<String> uploadImage(@PathVariable Long movieId, @RequestParam("image") MultipartFile image) {
-        // Обробка завантаження зображення та збереження його у полі "image" об'єкта Movie
-        service.uploadMovieImage(movieId, image.getBytes());
-        return ResponseEntity.ok("Image uploaded successfully.");
-    }
+  private final MovieService service;
 
-    @GetMapping("/{movieId}/image")
-    public ResponseEntity<byte[]> getImage(@PathVariable Long movieId) {
-        byte[] imageBytes = service.getMovieImage(movieId);
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imageBytes);
-    }
-    @GetMapping
-    public ResponseEntity<Page<MovieResponse>> getAllByPagination(Pageable pageable) {
-        return ResponseEntity.ok(service.getAll(pageable));
-    }
-    @GetMapping("/title")
-    public ResponseEntity<Page<MovieResponse>> getByTitleContaining(Pageable pageable,@RequestParam("title") String title){
-        return ResponseEntity.ok(service.findByTitleContaining(title,pageable));
-    }
-    @GetMapping("/genre")
-    public ResponseEntity<Page<MovieResponse>> getByGenres(Pageable pageable,@RequestParam("genre") Genre genre){
-        return ResponseEntity.ok(service.findByGenre(genre,pageable));
-    }
+  @GetMapping
+  public ResponseEntity<Page<MovieResponse>> getAllMovies(Pageable pageable,
+      @Nullable @RequestParam(required = false) String title,
+      @Nullable @RequestParam(required = false) List<Long> genreIds) {
+    return ResponseEntity.ok(service.getAllMovies(pageable, title, genreIds));
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<MovieResponse> getMovieById(@PathVariable Long id) {
+    return ResponseEntity.ok(service.getMovieById(id));
+  }
+
+  @PostMapping
+  public ResponseEntity<MovieResponse> createMovie(@RequestBody CreateMovieRequest request) {
+    var movie = service.createMovie(request);
+    return new ResponseEntity<>(movie, HttpStatus.CREATED);
+  }
 }
