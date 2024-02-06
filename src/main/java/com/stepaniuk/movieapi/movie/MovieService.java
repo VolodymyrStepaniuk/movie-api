ca8b9be13c26d0ba59fb33f9409642faa015e575
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 13:40:28 +0200

    feat(movie): Rewritten create and getById methods
    Also rewritten getAllMovies to use specification instead creating specific methods in MovieRepo

--- src/main/java/com/stepaniuk/movieapi/movie/MovieService.java
+++ src/main/java/com/stepaniuk/movieapi/movie/MovieService.java
@@ -1,83 +1,81 @@
 package com.stepaniuk.movieapi.movie;
 
-import com.stepaniuk.movieapi.exceptions.MovieNotFoundException;
+import com.stepaniuk.movieapi.director.DirectorRepository;
+import com.stepaniuk.movieapi.exceptions.movie.MovieNotFoundException;
+import com.stepaniuk.movieapi.country.CountryRepository;
+import com.stepaniuk.movieapi.genre.GenreRepository;
 import com.stepaniuk.movieapi.movie.payload.request.CreateMovieRequest;
-import com.stepaniuk.movieapi.movie.payload.response.MovieListResponse;
 import com.stepaniuk.movieapi.movie.payload.response.MovieResponse;
+import java.util.HashSet;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class MovieService  {
-    private final MovieRepository movieRepository;
-    private final MovieMapper movieMapper;
+public class MovieService {
 
-    public MovieResponse create(CreateMovieRequest createMovieRequest) {
-        Movie movie = new Movie();
+  private final MovieRepository movieRepository;
+  private final MovieMapper movieMapper;
+  private final DirectorRepository directorRepository;
+  private final GenreRepository genreRepository;
+  private final CountryRepository countryRepository;
 
-        movie.setTitle(createMovieRequest.getTitle());
-        movie.setYear(createMovieRequest.getYear());
-        movie.setProduction(createMovieRequest.getProduction());
-        movie.setGenres(createMovieRequest.getGenres());
-        movie.setDirector(createMovieRequest.getDirector());
-        movie.setDuration(createMovieRequest.getDuration());
-        movie.setLanguage(createMovieRequest.getLanguage());
-        movie.setDescription(createMovieRequest.getDescription());
+  public MovieResponse createMovie(CreateMovieRequest createMovieRequest) {
+    Movie movie = new Movie();
 
-        movieRepository.save(movie);
+    var directorIds = createMovieRequest.getDirectorIds();
+    var directors = directorRepository.findAllById(directorIds);
 
-        return movieMapper.toResponse(movie);
+    var genreIds = createMovieRequest.getGenreIds();
+    var genres = genreRepository.findAllById(genreIds);
+
+    var countryIds = createMovieRequest.getProducingCountryIds();
+    var countries = countryRepository.findAllById(countryIds);
+
+    movie.setTitle(createMovieRequest.getTitle());
+    movie.setRequiredAge(createMovieRequest.getRequiredAge());
+    movie.setYear(createMovieRequest.getYear());
+    movie.setProducingCountries(new HashSet<>(countries));
+    movie.setGenres(new HashSet<>(genres));
+    movie.setDirectors(new HashSet<>(directors));
+    movie.setDuration(createMovieRequest.getDuration());
+    movie.setLanguage(createMovieRequest.getLanguage());
+    movie.setDescription(createMovieRequest.getDescription());
+    movie.setImageUrls(createMovieRequest.getImageUrls());
+
+    var savedMovie = movieRepository.save(movie);
+
+    return movieMapper.toResponse(savedMovie);
+  }
+
+  public Page<MovieResponse> getAllMovies(Pageable pageable, @Nullable String title,
+      @Nullable List<Long> genreIds) {
+
+    Specification<Movie> specification = Specification.where(null);
+    if (title != null && !title.isEmpty()) {
+      specification = specification.and((root, query, criteriaBuilder) -> criteriaBuilder
+          .like(root.get("title"), title + "%")
+      );
     }
 
-
-    public MovieListResponse getAll() {
-        var listOfMovieResponse = movieRepository.findAll().stream().map(movieMapper::toResponse).toList();
-        return new MovieListResponse(listOfMovieResponse,listOfMovieResponse.size());
-    }
-    public Page<MovieResponse> getAll(Pageable pageable) {
-        return movieRepository.findAll(pageable).map(movieMapper::toResponse);
-    }
-
-    public MovieResponse getByIdToResponse(Long id) {
-        return movieMapper.toResponse(getById(id));
+    if (genreIds != null && !genreIds.isEmpty()) {
+      for (Long genreId : genreIds) {
+        Specification<Movie> genreSpecification = (root, query, criteriaBuilder) ->
+            root.join("genres").get("id").in(genreId);
+        specification = specification.and(genreSpecification);
+      }
     }
 
-    private Movie getById(Long id) {
-        return movieRepository.findById(id)
-                .orElseThrow(MovieNotFoundException::new);
-    }
-    public void uploadMovieImage(Long movieId, byte[] image) {
-        Movie movie = getById(movieId);
-        movie.setImage(image);
-        movieRepository.save(movie);
-    }
+    return movieRepository.findAll(specification, pageable).map(movieMapper::toResponse);
+  }
 
-    public byte[] getMovieImage(Long movieId) {
-        Movie movie = getById(movieId);
-        byte[] image = movie.getImage();
-        if(image.length==0){
-            throw new MovieNotFoundException();
-        }
-        return image;
-    }
-
-    public Page<MovieResponse> findByTitleContaining(String title, Pageable pageable){
-        Page<Movie> movie = movieRepository.findByTitleContaining(title, pageable);
-        if(movie.isEmpty()){
-            throw new MovieNotFoundException("No movies with title "+title+" found.");
-        }
-        return movie.map(movieMapper::toResponse);
-    }
-    public Page<MovieResponse> findByGenre(Genre genre, Pageable pageable){
-        Page<Movie> movie = movieRepository.findByGenre(genre, pageable);
-        if(movie.isEmpty()){
-            throw new MovieNotFoundException("No movies with genre "+genre+" found.");
-        }
-        return movie.map(movieMapper::toResponse);
-    }
+  public MovieResponse getMovieById(Long id) {
+    return movieMapper.toResponse(movieRepository.findById(id)
+        .orElseThrow(() -> new MovieNotFoundException(id)));
+  }
 }
