097c97ec344412176f1157a9d30663472aeceede
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 13:35:35 +0200

    feat(movie): Rewritten CreateMovieRequest
    New fields required for entity creation have been added

--- src/main/java/com/stepaniuk/movieapi/movie/payload/request/CreateMovieRequest.java
+++ src/main/java/com/stepaniuk/movieapi/movie/payload/request/CreateMovieRequest.java
@@ -1,19 +1,42 @@
 package com.stepaniuk.movieapi.movie.payload.request;
 
-import com.stepaniuk.movieapi.movie.Genre;
 import jakarta.validation.constraints.NotNull;
-import lombok.Value;
-
-
+import jakarta.validation.constraints.Size;
+import java.time.Duration;
 import java.util.List;
-@Value
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class CreateMovieRequest {
-    @NotNull String title;
-    int year;
-    String production;
-    List<Genre> genres;
-    @NotNull String director;
-    double duration;
-    String language;
-    String description;
+    @NotNull
+    private String title;
+    @NotNull
+    private Integer requiredAge;
+    @NotNull
+    private Integer year;
+    @NotNull
+    @Size(min = 1)
+    private List<Long> producingCountryIds;
+    @NotNull
+    @Size(min = 1)
+    private List<Long> genreIds;
+    @NotNull
+    @Size(min = 1)
+    private List<Long> directorIds;
+    @NotNull
+    private Duration duration;
+    @NotNull
+    private String language;
+    @NotNull
+    private String description;
+    private String trailerUrl;
+    private String videoUrl;
+    @NotNull
+    private List<String> imageUrls;
 }
