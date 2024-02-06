9d2623715029a6731f77844b2bb517590f8c3ec5
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 13:27:17 +0200

    feat(genre): Added GenreService

---
+++ src/main/java/com/stepaniuk/movieapi/genre/GenreService.java
@@ -0,0 +1,25 @@
+package com.stepaniuk.movieapi.genre;
+
+import com.stepaniuk.movieapi.exceptions.genre.GenreNotFoundException;
+import com.stepaniuk.movieapi.genre.payload.response.GenreResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GenreService {
+
+  private final GenreRepository genreRepository;
+  private final GenreMapper genreMapper;
+
+  public Page<GenreResponse> getAllGenres(Pageable pageable) {
+    return genreRepository.findAll(pageable).map(genreMapper::toResponse);
+  }
+
+  public GenreResponse getGenreById(Long id) {
+    return genreMapper.toResponse(
+        genreRepository.findById(id).orElseThrow(() -> new GenreNotFoundException(id)));
+  }
+}
