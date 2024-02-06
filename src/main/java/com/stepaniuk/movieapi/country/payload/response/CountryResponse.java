a4af94b46cf32ec0dc31a6d9635b9e49bfaad910
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 13:14:41 +0200

    feat(country): Added CountryResponse

---
+++ src/main/java/com/stepaniuk/movieapi/country/payload/response/CountryResponse.java
@@ -0,0 +1,22 @@
+package com.stepaniuk.movieapi.country.payload.response;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class CountryResponse {
+
+  @NotNull
+  private Long id;
+  @NotNull
+  private String name;
+  @NotNull
+  private String imageUrl;
+}
+
