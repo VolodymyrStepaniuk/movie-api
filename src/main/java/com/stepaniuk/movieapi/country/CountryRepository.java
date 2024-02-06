e04f0cb2b242be17847a99cf1f1a9bd7168cc15d
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 13:13:45 +0200

    feat(country): Added CountryRepository

---
+++ src/main/java/com/stepaniuk/movieapi/country/CountryRepository.java
@@ -0,0 +1,11 @@
+package com.stepaniuk.movieapi.country;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long>,
+    JpaSpecificationExecutor<Country> {
+
+}
