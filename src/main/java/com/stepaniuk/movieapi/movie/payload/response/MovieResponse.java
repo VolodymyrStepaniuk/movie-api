93816307be663fb3c8f9ee9f98e556d94fa0bea6
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 13:34:14 +0200

    feat(movie): Rewritten MovieResponse
    New fields that appeared in the entity were added

--- src/main/java/com/stepaniuk/movieapi/movie/payload/response/MovieResponse.java
+++ src/main/java/com/stepaniuk/movieapi/movie/payload/response/MovieResponse.java
@@ -1,20 +1,48 @@
 package com.stepaniuk.movieapi.movie.payload.response;
 
-import com.stepaniuk.movieapi.movie.Genre;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
+import com.stepaniuk.movieapi.country.payload.response.CountryResponse;
+import com.stepaniuk.movieapi.director.payload.response.DirectorResponse;
+import com.stepaniuk.movieapi.genre.payload.response.GenreResponse;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.Duration;
 import java.util.List;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
 @Getter
 @AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class MovieResponse {
+    @NotNull
     private Long id;
+    @NotNull
     private String title;
+    @NotNull
+    private Integer requiredAge;
+    @NotNull
     private Integer year;
-    private String production;
-    private List<Genre> genres;
-    private String director;
-    private Double duration;
+    @NotNull
+    @Size(min = 1)
+    private Set<CountryResponse> producingCountries;
+    @NotNull
+    @Size(min = 1)
+    private Set<GenreResponse> genres;
+    @NotNull
+    @Size(min = 1)
+    private Set<DirectorResponse> directors;
+    @NotNull
+    private Duration duration;
+    @NotNull
     private String language;
+    @NotNull
     private String description;
+    private String trailerUrl;
+    private String videoUrl;
+    @NotNull
+    private List<String> imageUrls;
 }
