d7388c1de12a5470b30afab8e965b6e977fa7dd4
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 13:19:35 +0200

    feat(director): Added DirectorRepository

---
+++ src/main/java/com/stepaniuk/movieapi/director/DirectorRepository.java
@@ -0,0 +1,11 @@
+package com.stepaniuk.movieapi.director;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DirectorRepository extends JpaRepository<Director, Long>,
+    JpaSpecificationExecutor<Director> {
+
+}
