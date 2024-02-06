49a59bb4e1209a53488a98e87ddb1123fc16d028
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 13:45:32 +0200

    ref(project): Changed code alignment

--- src/main/java/com/stepaniuk/movieapi/shared/InstantMapper.java
+++ src/main/java/com/stepaniuk/movieapi/shared/InstantMapper.java
@@ -6,11 +6,11 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface InstantMapper {
 
-    default Long toLong(java.time.Instant instant) {
-        return instant.toEpochMilli();
-    }
+  default Long toLong(java.time.Instant instant) {
+    return instant.toEpochMilli();
+  }
 
-    default java.time.Instant toInstant(Long epochMilli) {
-        return java.time.Instant.ofEpochMilli(epochMilli);
-    }
+  default java.time.Instant toInstant(Long epochMilli) {
+    return java.time.Instant.ofEpochMilli(epochMilli);
+  }
 }
